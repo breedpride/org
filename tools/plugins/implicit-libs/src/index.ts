a8086380018a5f1f631395626dc2bd6eaf02fb7d
Sergey Glova <sergey.glova@gmail.com>
2024-12-26 16:40:15 +0200

    add initial implementation of cart and catalog UI components with tests

---
+++ tools/plugins/implicit-libs/src/index.ts
@@ -0,0 +1,134 @@
+import {
+  CreateNodesResultV2,
+  CreateNodesV2,
+  ProjectConfiguration,
+} from '@nx/devkit';
+import { dirname, join } from 'node:path/posix';
+import { Optional } from 'nx/src/project-graph/plugins';
+import { getProjectInfo, hasFileMatching, hasIndexInParentTree } from './utils';
+
+export const createNodesV2: CreateNodesV2 = [
+  'libs/**/index.ts',
+  async (indexPathList, _, { workspaceRoot }): Promise<CreateNodesResultV2> => {
+    const results = await Promise.all(
+      indexPathList.map((indexPath) =>
+        createImplicitLibProjectConfig(indexPath, { workspaceRoot })
+      )
+    );
+
+    return results
+      .filter(isDefined)
+      .map(({ indexPath, projectConfiguration, projectPath }) => [
+        indexPath,
+        {
+          projects: {
+            [projectPath]: projectConfiguration,
+          },
+        },
+      ]);
+  },
+];
+
+async function createImplicitLibProjectConfig(
+  indexPath: string,
+  { workspaceRoot }: { workspaceRoot: string }
+) {
+  const projectPath = dirname(indexPath);
+  const projectRoot = join(workspaceRoot, projectPath);
+  if (await hasIndexInParentTree(projectRoot)) {
+    return;
+  }
+
+  const projectInfo = getProjectInfo(projectPath);
+  if (projectInfo === undefined) {
+    return;
+  }
+
+  const { name, platform, scope, type } = projectInfo;
+  const projectName = name ? `${name}-${type}` : type;
+  const hasTests = await hasFileMatching(projectRoot, '**/*.spec.ts');
+  return {
+    indexPath,
+    projectPath,
+    projectConfiguration: {
+      name: `${platform}-${scope}-${projectName}`,
+      sourceRoot: projectPath,
+      projectType: 'library',
+      tags: [`platform:${platform}`, `scope:${scope}`, `type:${type}`],
+      targets: {
+        ...createLintTarget(projectPath),
+        ...(hasTests ? createTestTarget(projectPath) : {}),
+      },
+    } satisfies Optional<ProjectConfiguration, 'root'>,
+  };
+}
+
+function createLintTarget(
+  projectPath: string
+): ProjectConfiguration['targets'] {
+  let currentPath = join('{workspaceRoot}', projectPath);
+  let eslintConfigPaths: string[] = [];
+  while (currentPath !== '.') {
+    eslintConfigPaths = [
+      ...eslintConfigPaths,
+      join(currentPath, 'eslint.config.js'),
+    ];
+    currentPath = dirname(currentPath);
+  }
+
+  return {
+    lint: {
+      command: 'eslint .',
+      options: {
+        cwd: projectPath,
+      },
+      metadata: {
+        technologies: ['eslint'],
+      },
+      cache: true,
+      inputs: [
+        'default',
+        '^default',
+        ...eslintConfigPaths,
+        '{workspaceRoot}/tools/eslint-rules/**/*',
+        {
+          externalDependencies: ['eslint'],
+        },
+      ],
+      outputs: ['{options.outputFile}'],
+    },
+  };
+}
+
+function createTestTarget(
+  projectPath: string
+): ProjectConfiguration['targets'] {
+  return {
+    test: {
+      command: 'vitest',
+      options: {
+        cwd: projectPath,
+        root: '.',
+      },
+      metadata: {
+        technologies: ['vitest'],
+      },
+      cache: true,
+      inputs: [
+        'default',
+        '^production',
+        {
+          externalDependencies: ['vitest'],
+        },
+        {
+          env: 'CI',
+        },
+      ],
+      outputs: [`{workspaceRoot}/coverage/${projectPath}`],
+    },
+  };
+}
+
+function isDefined<T>(value: T | undefined): value is T {
+  return value !== undefined;
+}
---
+++ tools/plugins/implicit-libs/src/utils.ts
@@ -0,0 +1,91 @@
+import { stat } from 'node:fs/promises';
+import { dirname, join } from 'node:path/posix';
+import { globIterate } from 'glob';
+import { logger } from '@nx/devkit';
+
+/**
+ * Checks if there are `index.ts` files in parent folders.
+ * This is useful to exclude folders with `index.ts` which are inside other libs.
+ */
+export async function hasIndexInParentTree(
+  folderPath: string
+): Promise<boolean> {
+  let previousFolderPath: string;
+
+  do {
+    previousFolderPath = folderPath;
+    folderPath = dirname(folderPath);
+    try {
+      await stat(join(folderPath, 'index.ts'));
+      return true;
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        'code' in error &&
+        error.code === 'ENOENT'
+      ) {
+        continue;
+      }
+      throw error;
+    }
+  } while (folderPath !== previousFolderPath);
+
+  return false;
+}
+
+export function getProjectInfo(projectPath: string):
+  | {
+      platform: string;
+      scope: string;
+      type: string;
+      name?: string;
+    }
+  | undefined {
+  const parts = projectPath.split('/');
+
+  if (parts.length !== 4) {
+    logger.warn(`Invalid project path ${projectPath}`);
+    return;
+  }
+  const [platform, scope, nameAndType] = parts.slice(-3);
+  const nameAndTypeParts = nameAndType.split('-');
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const type = nameAndTypeParts.at(-1)!;
+  const name =
+    nameAndTypeParts.length > 1
+      ? nameAndTypeParts.slice(0, -1).join('-')
+      : undefined;
+
+  if (!allowedLibraryTypes.includes(type)) {
+    logger.warn(
+      `Invalid project path ${projectPath}. Last folder should be one of the allowed types: ${allowedLibraryTypes}`
+    );
+    return;
+  }
+
+  return {
+    name,
+    platform,
+    scope,
+    type,
+  };
+}
+
+const allowedLibraryTypes = [
+  'domain',
+  'feature',
+  'infra',
+  'model',
+  'ui',
+  'utils',
+];
+
+export async function hasFileMatching(
+  path: string,
+  globPattern: string
+): Promise<boolean> {
+  const { done } = await globIterate(globPattern, {
+    cwd: path,
+  }).next();
+  return !done;
+}
