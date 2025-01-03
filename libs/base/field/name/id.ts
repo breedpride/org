574c0d8c03ab491ab24b05be73702eaaa15a5cf8
Sergey Glova <sergey.glova@gmail.com>
2025-01-03 18:53:13 +0200

    feat: add new index files and update tsconfig for improved module exports and type management

--- libs/base/field/name/id.ts
+++ libs/base/field/name/id.ts
@@ -1,7 +1,5 @@
-
 // ----------------- GENERATED FOR CHANGES-----------
 // Changes will NOT be overwritten
 // ------------------------------------------------
 
-        export const Id_FieldName = [ {"id": 'Id'} as const ] as const ;
-        
+export const Id_FieldName = [{ id: 'Id' } as const] as const;
--- libs/shared/libs/index.ts
+++ libs/shared/libs/index.ts
@@ -1,4 +1,4 @@
 // @index(['./**/*.{ts,tsx}', './*/index.{ts,tsx}'], f => `export * from '${f.path.replace(/\/index$/, '')}'`)
-
+export * from './types';
+export * from './types/schema-query';
 // @endindex
-export * from './common/types/index';
