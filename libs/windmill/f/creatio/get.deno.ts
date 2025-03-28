4f5fdca6d044f2f5c1a1c5237e32e75ef704a801
dreamer1986 <sergey.glova@dogarray.com>
2025-03-28 14:04:52 +0200

    feat: implement data retrieval functions for Creatio API with error handling

---
+++ libs/windmill/f/creatio/get.deno.ts
@@ -0,0 +1,27 @@
+import * as wmill from "npm:windmill-client@1.475.1";
+
+export async function main(url: string) {
+  const token = await wmill.getVariable("f/creatio/creatio_token");
+  const resp = await fetch(
+    url,
+    {
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      } as HeadersInit,
+      method: "GET",
+    }
+  );
+  const json = JSON.parse(await resp.text()).result.data;
+  if (json === undefined) {
+    throw new Error("No data found");
+  }
+  if (json.result.isSuccess) {
+    if ( json.result.isSerializedData) {
+        return JSON.parse(json.result.data);
+    }
+  }
+
+  return json;
+}
+
---
+++ libs/windmill/f/creatio/get_dictionaries.deno.ts
@@ -0,0 +1,5 @@
+
+import {main as get} from "./get.deno.ts";
+async function main() {
+    return get('https://dogarray.com/0/ServiceModel/BreedprideAdminApi/GetLookupUpdateData');
+}
