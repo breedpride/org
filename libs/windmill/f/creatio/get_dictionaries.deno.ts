f405cfc24c0757e9521acea81a323e38293f4be5
dreamer1986 <sergey.glova@dogarray.com>
2025-03-28 14:14:10 +0200

    fix: update import path and ensure await for data retrieval in get_dictionaries

--- libs/windmill/f/creatio/get_dictionaries.deno.ts
+++ libs/windmill/f/creatio/get_dictionaries.deno.ts
@@ -1,5 +1,5 @@
 
-import {main as get} from "./get.deno.ts";
+import {main as get} from "/f/creatio/get.deno.ts";
 async function main() {
-    return get('https://dogarray.com/0/ServiceModel/BreedprideAdminApi/GetLookupUpdateData');
+    return await get('https://dogarray.com/0/ServiceModel/BreedprideAdminApi/GetLookupUpdateData');
 }
