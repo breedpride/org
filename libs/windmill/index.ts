624c7709116f043f509ba12cb624d154991e60e3
dreamer1986 <sergey.glova@dogarray.com>
2025-03-30 16:37:35 +0300

    feat: export model fields and types from Creatio in windmill index

--- libs/windmill/index.ts
+++ libs/windmill/index.ts
@@ -18,6 +18,8 @@
 // export * from './f/supabase/deno_init.deno';
 export * from './f/supabase/enums_consts.deno';
 export * from './f/consts/index';
+export * from './f/creatio/model_fields';
+export * from './f/creatio/model_types';
 // export * from './f/supabase/insert.deno';
 // export * from './u/annamaliyenko/bullish_script';
 // export * from './u/annamaliyenko/important_script';
