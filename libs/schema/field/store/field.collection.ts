569645befe8752d6663ded2ba38d8da7a534a7b4
dreamer1986 <sergey.glova@dogarray.com>
2025-04-01 00:20:09 +0300

    refactor: remove unused field collection and token files to clean up the codebase

--- libs/schema/field/store/field.collection.ts
+++
@@ -1,3 +0,0 @@
-import { NullableField } from '@bh/types';
-
-export const DEFAULT_FIELD_CONFIG: NullableField<any> = null;
--- libs/schema/field/store/index.ts
+++
@@ -1,4 +0,0 @@
-// @index(['./**/*.{ts,tsx}', './*/index.{ts,tsx}'], f => `export * from '${f.path.replace(/\/index$/, '')}'`)
-export * from './field.collection';
-export * from './field.token';
-// @endindex
